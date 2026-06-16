27cb530668ebac25f08d759fec5f25d3941210e9
brycee916 <brycechao27@gmail.com>
2026-06-15 21:40:16 -0700

    get one employee

--- src/main/java/com/example/Payroll/EmployeeController.java
+++ src/main/java/com/example/Payroll/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.Payroll;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,17 @@ class EmployeeController {
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee){
         return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id){
+        return repository.findById(id)
+            .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
+        
     }
 
 }
