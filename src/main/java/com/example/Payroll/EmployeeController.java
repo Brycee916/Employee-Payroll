d99f911e2daff17104fb60cc4c370c1d7f34cec1
brycee916 <brycechao27@gmail.com>
2026-06-15 21:16:48 -0700

    added all() function

--- src/main/java/com/example/Payroll/EmployeeController.java
+++ src/main/java/com/example/Payroll/EmployeeController.java
@@ -1,5 +1,23 @@
 package com.example.Payroll;
 
-public class EmployeeController {
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all(){
+        return repository.findAll();
+    }
+
+    
 
 }
